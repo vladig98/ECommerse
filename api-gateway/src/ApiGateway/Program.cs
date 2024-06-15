88b7fa0e0cd8c23a9a0f385b7b2d460dcd2ae3cf
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-06-15 20:45:15 +0300

    repo init

---
+++ api-gateway/src/ApiGateway/Program.cs
@@ -0,0 +1,18 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseOcelot().Wait();
+app.Run();
