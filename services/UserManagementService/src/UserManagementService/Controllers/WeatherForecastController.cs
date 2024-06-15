ee00ebd28399dd0359288e73f1066f8a6ac4f60c
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-06-15 20:50:08 +0300

    adding userservice

---
+++ services/UserManagementService/src/UserManagementService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+    }
+}
