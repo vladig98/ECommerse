2e8614a7fe5b05a1f0d5a698a733ee6fa9954335
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2026-08-06 12:20:11 +0300

    Delete ECommerce.Catalog/Models/VariantAttribute.cs

--- ECommerce.Catalog/Models/VariantAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ECommerce.Catalog.Models;
-
-public class VariantAttribute : BaseModel
-{
-    public string Name { get; set; } = string.Empty;
-    public string Value { get; set; } = string.Empty;
-
-    public ICollection<ProductVariantAttribute> ProductVariants { get; set; } = [];
-}
